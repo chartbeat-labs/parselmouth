a74cb7bbb6a109cdf49780e88230d5a8dbf990a8
Justin Mazur <justin.mazur@chartbeat.com>
2015-11-04 10:53:21 -0500

    Update to version 1.0.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='parselmouth',
     packages=find_packages(),
-    version='0.5',
+    version='1.0.0',
     description=(
         'An object oriented interface for ad provider services in python'
     ),
     author='Paul Kiernan, Justin Mazur',
     author_email='paulkiernan1@gmail.com, justindmazur@gmail.com',
     url='https://github.com/chartbeat-labs/parselmouth',
-    download_url='https://github.com/chartbeat-labs/parselmouth/tarball/0.5',
+    download_url='https://github.com/chartbeat-labs/parselmouth/tarball/1.0.0',
     keywords = ['googleads', 'dfp', 'ad server', 'parselmouth', 'ad provider'],
     classifiers = [],
     install_requires=[
