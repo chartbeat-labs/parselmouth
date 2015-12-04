a9ecce82b6c00d00d15fb4049343274925af8ee3
Paul Kiernan <paul@chartbeat.com>
2015-12-04 17:21:47 -0500

    Update tarball location

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Paul Kiernan, Justin Mazur',
     author_email='paulkiernan1@gmail.com, justindmazur@gmail.com',
     url='https://github.com/chartbeat-labs/parselmouth',
-    download_url='https://github.com/chartbeat-labs/parselmouth/tarball/1.0.1',
+    download_url='https://github.com/chartbeat-labs/parselmouth/tarball/1.0.2',
     keywords = ['googleads', 'dfp', 'ad server', 'parselmouth', 'ad provider'],
     classifiers = [],
     install_requires=[
