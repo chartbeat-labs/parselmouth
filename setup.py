2b5c4b4a2cd95d23efcf48f618392f93725fc20c
Paul Kiernan <paul@chartbeat.com>
2015-12-18 17:33:27 -0500

    Bump package version number

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='parselmouth',
     packages=find_packages(),
-    version='1.0.3',
+    version='1.1.1',
     description=(
         'An object oriented interface for ad provider services in python'
     ),
     author='Paul Kiernan, Justin Mazur',
     author_email='paulkiernan1@gmail.com, justindmazur@gmail.com',
     url='https://github.com/chartbeat-labs/parselmouth',
-    download_url='https://github.com/chartbeat-labs/parselmouth/tarball/1.0.3',
+    download_url='https://github.com/chartbeat-labs/parselmouth/tarball/1.1.1',
     keywords = ['googleads', 'dfp', 'ad server', 'parselmouth', 'ad provider'],
     classifiers = [],
     install_requires=[
